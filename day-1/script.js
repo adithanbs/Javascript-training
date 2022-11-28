55b263c2c8885af1af922b6045e8e4466d2978df
Adithan.B <adithan.baskar@dreamguystech.com>
2022-11-28 09:51:16 +0530

    day-1 commit

---
+++ day-1/script.js
@@ -0,0 +1,45 @@
+// console.log("hello world");
+// Comments
+
+// in-line Comment and mulite-line comments
+
+// --This is in-line comments
+/* 
+--This is the mulit-line comment--
+
+Lorem ipsum dolor sit amet, consectetur
+adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation 
+ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
+in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+deserunt mollit anim id est laborum
+*/
+
+
+// Data Type :
+/*
+Thay are two types of datatypes
+  1,Primative data type 
+   Number,String,Boolean,undefind,null,BigInt
+  2,Non-1,Primative data type 
+    Array and object
+ */
+
+
+// Variables : 
+
+/*
+Thay are three Type of variable declaration
+  1,var 2,let 3,const
+ 
+*/
+
+// The difference between  declaration-variable and Assigning-variable
+
+//declaration-variable
+var a; 
+// Assigning-variable
+var b = 10;
+var c = 20;
+var C = 30;
+console.log(b + C);
