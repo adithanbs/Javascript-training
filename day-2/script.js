618d8f46dc0f4097d4e6a2da0d6c1a1eb5d31b58
Adithan.B <adithan.baskar@dreamguystech.com>
2022-11-29 09:35:22 +0530

    29-11

---
+++ day-2/script.js
@@ -0,0 +1,30 @@
+var a = 20 ;
+var b = 10 ;
+var c = "sarth";
+var d = false;
+
+
+let arr = [1,2,3,"sting",true]
+
+
+
+
+let obj = {
+    name:"kumar",
+    age:2,
+    val :true
+}
+
+
+// console.log(arr[3]);
+// console.log(obj.age);
+// console.log(arr.length);
+
+for (let i = 0; i < arr.length ; i++) {
+    console.log(arr[i]);
+  }
+
+  
+// for(var i = 0 ; i < 10 ; i+1 ){
+//     console.log(i);
+//  }
