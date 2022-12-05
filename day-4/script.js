e3ba71ebe1e3122efb2e360293c1958c852e44a5
Adithan.B <adithan.baskar@dreamguystech.com>
2022-12-05 09:15:22 +0530

    day-5

--- day-4/script.js
+++ day-4/script.js
@@ -1,95 +1,140 @@
-let sampleData = [1,2,4,4,5,7,8]
-let  odd = [];
-let even = [];
+// let sampleData = [1,2,4,4,5,7,8]
+// let  odd = [];
+// let even = [];
 
-for(i = 0 ; i < sampleData.length; i ++) {
+// for(i = 0 ; i < sampleData.length; i ++) {
 
- if(sampleData[i] % 2 === 0) {
-   even.push(sampleData[i])
-  // sampleData[i]
- }else{
-   odd.push(sampleData[i])
- }
+//  if(sampleData[i] % 2 === 0) {
+//    even.push(sampleData[i])
+//   // sampleData[i]
+//  }else{
+//    odd.push(sampleData[i])
+//  }
 
-}
+// }
 
- console.log("odd",odd);
-console.log("even",even);
+//  console.log("odd",odd);
+// console.log("even",even);
 
-// switch
+// // switch
 
-switch ("value2") {
+// switch ("value2") {
 
-    case "value1":
-        console.log("first value");
-        break;
+//     case "value1":
+//         console.log("first value");
+//         break;
   
-    case "value2":
-         console.log("second value");
-        break;     
+//     case "value2":
+//          console.log("second value");
+//         break;     
    
-    default:        
-       console.log ('not found');
-        break;
+//     default:        
+//        console.log ('not found');
+//         break;
+// }
+
+// switch(variable/expression) {
+//   case value1:  
+//       // body of case 1
+//       break;
+
+//   case value2:  
+//       // body of case 2
+//       break;
+
+//   case valueN:
+//       // body of case N
+//       break;
+
+//   default:
+//       // body of default
+// }
+
+// // program using switch statement
+// let a = 2;
+
+// switch (a) {
+
+//   case 1:
+//       a = 'one';
+//       break;
+//   case 2:
+//       a = 'two';
+//       break;
+//   default:
+//       a = 'not found';
+//       break;
+// }
+// console.log(`The value is ${a}`);
+
+
+// // Ex Function
+
+// function funName(param1) {
+//   let sampleData = param1
+//  let  odd = [];
+//  let even = [];
+ 
+//  for(i = 0 ; i < sampleData.length; i ++) {
+ 
+//   if(sampleData[i] % 2 === 0) {
+//     even.push(sampleData[i])
+//    // sampleData[i]
+//   }else{
+//     odd.push(sampleData[i])
+//   }
+ 
+//  }
+ 
+//   console.log("odd",odd);
+//  console.log("even",even);
+//  }
+ 
+//  funName([2,3,4,7,9,10,11,12]);
+ 
+//  funName([2,3,4,7,9,10,11,33,44]);
+
+
+// {
+//   let a = 10;
+//   const c = 20
+//   var b = 10 ;
+// }
+
+//  console.log(a);
+// console.log(b);
+// console.log();
+
+// var a = 20
+// var a = 30
+// console.log(a);
+
+// let d  
+//  d = 20 
+// console.log(d);
+
+// const k = 243
+
+
+// {
+//   var t = 10
+// } 
+
+// console.log(t)
+
+function nam(param1,param2) {
+  console.log(param1+param2);
 }
 
-switch(variable/expression) {
-  case value1:  
-      // body of case 1
-      break;
+nam(15,30)
 
-  case value2:  
-      // body of case 2
-      break;
-
-  case valueN:
-      // body of case N
-      break;
-
-  default:
-      // body of default
+let fun = function (param1,param2) {
+  console.log(param1+param2);
 }
 
-// program using switch statement
-let a = 2;
+fun(30,6)
 
-switch (a) {
+const arrow = () => {
 
-  case 1:
-      a = 'one';
-      break;
-  case 2:
-      a = 'two';
-      break;
-  default:
-      a = 'not found';
-      break;
 }
-console.log(`The value is ${a}`);
 
-
-// Ex Function
-
-function funName(param1) {
-  let sampleData = param1
- let  odd = [];
- let even = [];
- 
- for(i = 0 ; i < sampleData.length; i ++) {
- 
-  if(sampleData[i] % 2 === 0) {
-    even.push(sampleData[i])
-   // sampleData[i]
-  }else{
-    odd.push(sampleData[i])
-  }
- 
- }
- 
-  console.log("odd",odd);
- console.log("even",even);
- }
- 
- funName([2,3,4,7,9,10,11,12]);
- 
- funName([2,3,4,7,9,10,11,33,44]);
